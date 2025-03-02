aa5fb491ac1e52be9673b9df8218d083c73c997b
JohnRTitor <masumrezarock100@gmail.com>
2025-03-03 00:45:16 +0530

    simplify

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -15,12 +15,10 @@ contract StorageFactory {
     function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
         // For interacting with contracts
         // We need, address, and ABI - Application Binary Interface
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
-        simpleStorage.store(_simpleStorageNumber);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
     }
 
     function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
-        return simpleStorage.retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 }
