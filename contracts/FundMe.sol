55151c01c5bb06148df98209624b9e0055b8269c
JohnRTitor <masumrezarock100@gmail.com>
2025-03-04 16:57:30 +0530

    gas optimization, change to constant and immutable variables

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,6 +2,7 @@
 // Get funds from users
 // Withdraw funds
 // Set a minimum funding value in USD
+// We need to reduce gas cost
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
@@ -11,20 +12,24 @@ import "./PriceConverter.sol";
 contract FundMe {
     // we need to specify this for it to work with uint256 objects
     using PriceConverter for uint256;
-    uint256 public minimumUsd = 50 * 1e18;
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
+    // 21,415 gas - constant
+    // 23,515 gas - non-constant
 
     address[] public funders;
     mapping(address =>  uint256) public addressToAmountFunded;
 
-    address public owner;
+    address public immutable i_owner;
+    // 21,508 gas - immutable
+    // 23,644 gas - non-immutable
 
     constructor() {
         // Constructor gets immediately called after deploying a contract
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable  {
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
@@ -43,7 +48,7 @@ contract FundMe {
 
     // this is custom modifier, it is executed first if added to a functon declaration
     modifier onlyOwner {
-        require(msg.sender == owner, "Sender is not owner!");
+        require(msg.sender == i_owner, "Sender is not owner!");
         _; // do the rest of the code
     }
 }
